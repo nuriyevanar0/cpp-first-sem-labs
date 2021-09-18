7459f345cae0a347dd166c3851b4b79e00042aec
Anar <46101620+nuriyevanar0@users.noreply.github.com>
2021-09-18 12:01:46 +0400

    Add files via upload

---
+++ lab1ex10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    float degree;
+
+    cout << "Degree: ";
+    cin >> degree;
+
+    cout << "Radian is " << degree * 3.1416/180 ;   
+    return 0;
+}
