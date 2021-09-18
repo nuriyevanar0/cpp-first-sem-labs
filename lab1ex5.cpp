803557736f9ac0dde4d93982f11726a4c5c33e09
Anar <46101620+nuriyevanar0@users.noreply.github.com>
2021-09-18 12:00:31 +0400

    Add files via upload

---
+++ lab1ex5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int num1, num2 , num3;
+
+    cout << "Number #1: ";
+    cin >> num1;
+    cout << "Number #2: ";
+	cin >> num2;
+	cout << "Number #3: ";
+    cin >> num3;
+
+    cout << num1 << "\n";
+	cout << num2 << "\n";
+	cout << num3 << "\n";
+	cout << "\n";
+	cout << num3 << "\n";
+	cout << num2 << "\n";
+	cout << num1 << "\n";    
+    return 0;
+}
