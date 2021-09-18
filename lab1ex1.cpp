9c8ac4f21449588b34235dd52014c64d8aa07826
Anar <46101620+nuriyevanar0@users.noreply.github.com>
2021-09-18 11:57:01 +0400

    Add files via upload

---
+++ lab1ex1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{    
+    string name;
+
+    cout << "Hello. What is your name?: ";
+    cin >> name;
+
+    cout << "Hello " << name << "!";    
+    return 0;
+}
