7e938ba9dc6d3c409548ac25eac769fa294c4f84
Anar <46101620+nuriyevanar0@users.noreply.github.com>
2021-09-18 12:00:48 +0400

    Add files via upload

---
+++ lab1ex6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{    
+    string letter;
+
+    cout << "Please input a letter: ";
+    cin >> letter;
+    cout << "You wrote a letter " << letter;    
+    return 0;
+}
